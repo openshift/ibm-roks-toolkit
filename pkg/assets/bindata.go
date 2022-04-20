31ac27714310d7ec7619f963609e2ab00967070a
John McMeeking <jmcmeek@us.ibm.com>
2022-04-20 11:09:58 -0500

    Disable PodSecurity in v4.10
    Disable the PodSecury admission plugin for 4.10 to match OCP.
    
    Partially implements https://github.ibm.com/alchemy-containers/armada-update/issues/3032

--- pkg/assets/bindata.go
+++ pkg/assets/bindata.go
@@ -2219,6 +2219,8 @@ apiServerArguments:
   - /etc/kubernetes/config/serving-ca.crt
   cloud-provider:
   - "{{ .CloudProvider }}"
+  disable-admission-plugins:
+  - PodSecurity
   enable-admission-plugins:
   - CertificateApproval
   - CertificateSigning
