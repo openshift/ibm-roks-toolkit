b7a191c5eb91290052e53ae5fdebd4babd66f64f
Smitha Subbarao <smithasubbarao@Smithas-MacBook-Pro.local>
2022-06-01 16:02:57 -0500

    ROKS 4.9:Do not always pull ROKS toolkit images

--- pkg/assets/bindata.go
+++ pkg/assets/bindata.go
@@ -1705,7 +1705,7 @@ spec:
       containers:
         - name: cluster-version-operator
           image: {{ .ReleaseImage }}
-          imagePullPolicy: Always
+          imagePullPolicy: IfNotPresent
 {{- if .ClusterVersionOperatorSecurityContext }}
 {{- $securityContext := .ClusterVersionOperatorSecurityContext }}
           securityContext:
@@ -1758,7 +1758,7 @@ spec:
           securityContext:
             runAsUser: {{ $securityContext.RunAsUser }}
 {{- end }}
-          imagePullPolicy: Always
+          imagePullPolicy: IfNotPresent
           command:
             - "metrics-pusher"
           args:
@@ -2641,7 +2641,7 @@ spec:
           runAsUser: {{ $securityContext.RunAsUser }}
 {{- end }}
         image: {{ .ROKSMetricsImage }}
-        imagePullPolicy: Always
+        imagePullPolicy: IfNotPresent
         command:
           - "metrics-pusher"
         args:
@@ -5029,7 +5029,7 @@ spec:
           runAsNonRoot: {{ $securityContext.RunAsNonRoot }}
 {{- end }}
         image: {{ .ROKSMetricsImage }}
-        imagePullPolicy: Always
+        imagePullPolicy: IfNotPresent
         args:
         - "--alsologtostderr"
         - "--v=3"
@@ -5107,7 +5107,7 @@ spec:
           runAsNonRoot: {{ $securityContext.RunAsNonRoot }}
 {{- end }}
         image: {{ .ROKSMetricsImage }}
-        imagePullPolicy: Always
+        imagePullPolicy: IfNotPresent
         command:
         - pushgateway
         ports:
