056f4475b4fd2241a452abbedbc518ea649db124
Smitha Subbarao <smithasubbarao@Smithas-MacBook-Pro.local>
2022-06-10 13:43:45 -0500

    ROKS 4.11: Update ROKS toolkit image policies

--- pkg/assets/bindata.go
+++ pkg/assets/bindata.go
@@ -1705,7 +1705,7 @@ spec:
       containers:
         - name: cluster-version-operator
           image: {{ .ReleaseImage }}
-          imagePullPolicy: Always
+          imagePullPolicy: IfNotPresent
 {{- if .ClusterVersionOperatorSecurityContext }}
 {{- $securityContext := .ClusterVersionOperatorSecurityContext }}
           securityContext:
@@ -1758,7 +1758,7 @@ spec:
           securityContext:
             runAsUser: {{ $securityContext.RunAsUser }}
 {{- end }}
-          imagePullPolicy: Always
+          imagePullPolicy: IfNotPresent
           command:
             - "metrics-pusher"
           args:
@@ -2639,7 +2639,7 @@ spec:
           runAsUser: {{ $securityContext.RunAsUser }}
 {{- end }}
         image: {{ .ROKSMetricsImage }}
-        imagePullPolicy: Always
+        imagePullPolicy: IfNotPresent
         command:
           - "metrics-pusher"
         args:
@@ -5027,7 +5027,7 @@ spec:
           runAsNonRoot: {{ $securityContext.RunAsNonRoot }}
 {{- end }}
         image: {{ .ROKSMetricsImage }}
-        imagePullPolicy: Always
+        imagePullPolicy: IfNotPresent
         args:
         - "--alsologtostderr"
         - "--v=3"
@@ -5105,7 +5105,7 @@ spec:
           runAsNonRoot: {{ $securityContext.RunAsNonRoot }}
 {{- end }}
         image: {{ .ROKSMetricsImage }}
-        imagePullPolicy: Always
+        imagePullPolicy: IfNotPresent
         command:
         - pushgateway
         ports:
